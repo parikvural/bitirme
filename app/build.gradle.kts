95f67808303f7fe9fc949fff04991a8ebb87a3c3
parikvural <parikvural@gmail.com>
2025-05-03 22:02:17 +0300

    düzeltmeler ve birinci ekrandan ikinci ekrana geçiş

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.frkvrl.bitirme"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.frkvrl.bitirme"
--- app/src/main/java/com/frkvrl/bitirme/MainActivity.kt
+++ app/src/main/java/com/frkvrl/bitirme/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.frkvrl.bitirme
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun giris(view:View)
+    {
+        val intent = Intent(this,MainActivity2::class.java)
+        startActivity(intent)
     }
 }
